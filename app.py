c8fdf82216e25ba3af8e6dcd0c64e91c555bf929
super-alex22 <116815031+super-alex22@users.noreply.github.com>
2025-12-18 10:46:19 +0200

    Add age check for voting eligibility

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ if name:
     st.write(f"Hello {name}")
     if age:
         st.write(f"You are {age} years old")
+        if age>17:
+            st.write("You are of legal age")
+        else:
+            st.write("You are not old enough to vote")
